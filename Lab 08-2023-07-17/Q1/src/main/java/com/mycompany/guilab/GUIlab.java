af0146cb6b929da7c1d5f52a6bcac650f5c3f4f6
lakshikarajapaksha <135578245+lakshikarajapaksha@users.noreply.github.com>
2023-07-17 14:03:10 +0530

    Add files via upload

---
+++ Lab 08-2023-07-17/Q1/src/main/java/com/mycompany/guilab/GUIlab.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.guilab;
+
+
+public class GUIlab extends Bgcolor
+{
+
+    public static void main(String[] args) {
+        Bgcolor b=new Bgcolor();
+        b.show();
+       
+    }
+}
---
+++ Lab 08-2023-07-17/Q2/src/main/java/com/mycompany/clickme/ClickMe.java
@@ -0,0 +1,11 @@
+
+package com.mycompany.clickme;
+
+
+public class ClickMe extends Click{
+
+    public static void main(String[] args) {
+        Click c = new Click ();
+        c.show();
+    }
+}
